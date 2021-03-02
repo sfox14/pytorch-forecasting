da07d629f7d6c3346fc6fac5fc2caaef0c5294fb
Sean Fox <sean.fox@sydney.edu.au>
2021-03-02 17:59:17 +1100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='pytorch-forecasting',
+    version='0.8.3',
+    packages=['pytorch_forecasting'],
+    description='Forecasting timeseries with PyTorch - dataloaders, normalizers, metrics and models',
+    url='https://github.com/jdb78/pytorch-forecasting',
+    author='Jan Beitner',
+    license='MIT',
+    author_email='',
+    install_requires=[],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
